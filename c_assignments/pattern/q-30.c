9fb54f1ddd30da5398485df20f2f81aebc4aff41
ParvSuhagiya <parv.suhagiya.cg@gmail.com>
2025-09-15 13:02:01 +0530

    pattern 28 to 32

---
+++ c_assignments/pattern/q-30.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+    int n=5;
+    for (int i = 0; i < (n*2)-2; i+=2)
+    {
+        for (int j = 7; j >= i; j-=2)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k <= i; k++)
+        {
+            printf("%c", 65+k);
+        }
+        
+        printf("\n");
+    }
+
+    for (int i = (n*2)-2; i >= 0 ; i-=2)
+    {
+        for (int j = 7; j >= i; j-=2)
+        {
+            printf(" ");     
+        }
+        for (int k = 0; k <= i; k++)
+        {
+            printf("%c", 65+k);
+        }
+        
+        printf("\n");
+    }
+    
+    return 0;
+}
