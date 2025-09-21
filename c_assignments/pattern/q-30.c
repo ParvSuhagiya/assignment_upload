c0964dfe670967723652c95fefaac18377f85352
ParvSuhagiya <parv.suhagiya.cg@gmail.com>
2025-09-21 23:14:48 +0530

    fixed some questions

--- c_assignments/pattern/q-30.c
+++ c_assignments/pattern/q-30.c
@@ -3,39 +3,51 @@
 int main(){
     int n;
     scanf("%d", &n);
-    for (int i = 0; i < (n*2)-2; i+=2)
+
+    for (int i = 1; i < n; i++)
     {
-        for (int j = 7; j >= i; j-=2)
+        for (int j = n; j >= i; j--)
         {
-            printf(" ");
+            printf(" ");     
         }
-        for (int k = 0; k <= i; k++)
+        for (int k = 1; k <= i; k++)
         {
-            if (k == i)
+            if (k == i )
             {
-                for (int num = k; num >= 0; num--)
+                for (int num = i; num >= 1; num--)
                 {
-                    printf("%c", 65+num);
+                    printf("%c", 64+num);
                 }
             }
             else
-            {    
-                printf("%c", 65+k);
+            {
+                printf("%c", 64+k);     
             }
         }
         
         printf("\n");
     }
 
-    for (int i = (n*2)-2; i >= 0 ; i-=2)
+
+
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = 7; j >= i; j-=2)
+        for (int j = 1; j <= i; j++)
         {
             printf(" ");     
         }
-        for (int k = 0; k <= i; k++)
+        for (int k = 1; k <= n-i+1; k++)
         {
-            printf("%c", 65+k);
+            if (k == n-i+1)
+            {
+                for (int num = k; num >= 1; num--)
+                {
+                    printf("%c", 64+num);
+                }
+            }
+            else{
+                printf("%c", 64+k);     
+            }
         }
         
         printf("\n");
