936d22ee2fe2dc146e0ceab43823acbe126c5a56
ParvSuhagiya <parv.suhagiya.cg@gmail.com>
2025-09-22 09:19:56 +0530

    updated q-55

--- c_assignments/pattern/q-55.c
+++ c_assignments/pattern/q-55.c
@@ -1,42 +1,57 @@
 #include <stdio.h>
 
-int main()
-{
+int main(){
     int n;
-    scanf("%d",&n);
-    
-    for(int i=1;i<=n;i++)
+    scanf("%d", &n);
+
+    for (int i = 1; i < n; i++)
     {
-        for(int j=1;j<=n-i;j++)
+        for (int j = n; j >= i; j--)
         {
-            printf(" ");
+            printf(" ");     
         }
-        for(int j=1;j<=i;j++)
+        for (int k = 1; k <= i; k++)
         {
-            printf("%d",j);
+            if (k == i )
+            {
+                for (int num = i; num >= 1; num--)
+                {
+                    printf("%d", num);
+                }
+            }
+            else
+            {
+                printf("%d", k);     
+            }
         }
-        for(int j=i-1;j>=1;j--)
-        {
-            printf("%d",j);
-        }
-        printf(" \n");
-    }
-    for(int i=n;i>=1;i--)
-    {
-        for(int j=1;j<=n-i;j++)
-        {
-            printf(" ");
-        }
-        for(int j=1;j<=i;j++)
-        {
-            printf("%d",j);
-        }
-        for(int j=i-1;j>=1;j--)
-        {
-            printf("%d",j);
-        }
-        printf(" \n");
+        
+        printf("\n");
     }
 
+
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf(" ");     
+        }
+        for (int k = 1; k <= n-i+1; k++)
+        {
+            if (k == n-i+1)
+            {
+                for (int num = k; num >= 1; num--)
+                {
+                    printf("%d", num);
+                }
+            }
+            else{
+                printf("%d", k);     
+            }
+        }
+        
+        printf("\n");
+    }
+    
     return 0;
 }
