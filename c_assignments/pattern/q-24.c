dea8175563c384f7261ac10574879ce8aaaa053a
ParvSuhagiya <parv.suhagiya.cg@gmail.com>
2025-09-22 17:52:40 +0530

    improved logic in q-24,q-25

--- c_assignments/pattern/q-24.c
+++ c_assignments/pattern/q-24.c
@@ -3,23 +3,22 @@
 int main(){
     int n;
     scanf("%d", &n);
+    int count = 0;
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= i; j++)
+        count = 0;
+        for (int j = 1; j <= (2*i)-1; j++)
         {
-            if (j!=i)
+            if ( j <= i)
             {
-                printf("%d", j);
-            } 
-            else if (j==i)
-            {
-                for (int k = j; k > 0; k--)
-                {
-                    printf("%d", k);
-                }
-                
+                count++;
+                printf("%d", count);
             }
-            
+            else
+            {
+                count--;
+                printf("%d", count);
+            }
         }
         printf("\n");
     }
--- c_assignments/pattern/q-25.c
+++ c_assignments/pattern/q-25.c
@@ -3,24 +3,22 @@
 int main(){
     int n;
     scanf("%d", &n);
+    int count = 64;
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= i; j++)
+        count = 64;
+        for (int j = 1; j <= (2*i)-1; j++)
         {
-            if (j!=i)
+            if ( j <= i)
             {
-                printf("%c", 64+j);
+                count++;
+                printf("%c", count);
             }
-            
-            if (j==i)
+            else
             {
-                for (int k = j; k > 0; k--)
-                {
-                    printf("%c", 64+k);
-                }
-                
+                count--;
+                printf("%c", count);
             }
-            
         }
         printf("\n");
     }
