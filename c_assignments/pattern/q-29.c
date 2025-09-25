31dcd8d39ff74c3e083d136fd2727127259e9ef3
ParvSuhagiya <parv.suhagiya.cg@gmail.com>
2025-09-25 12:47:38 +0530

    added q-5 to q-8 (assets)

--- c_assignments/pattern/q-29.c
+++ c_assignments/pattern/q-29.c
@@ -1,35 +1,100 @@
+// #include <stdio.h>
+
+// int main(){
+//     int n;
+//     scanf("%d", &n);
+//     for (int i = 0; i < (n*2)-2; i+=2)
+//     {
+//         for (int j = 7; j >= i; j-=2)
+//         {
+//             printf(" ");
+//         }
+//         for (int k = 0; k <= i; k++)
+//         {
+//             printf("*");
+//         }
+        
+//         printf("\n");
+//     }
+
+//     for (int i = (n*2)-2; i >= 0 ; i-=2)
+//     {
+//         for (int j = 7; j >= i; j-=2)
+//         {
+//             printf(" ");     
+//         }
+//         for (int k = 0; k <= i; k++)
+//         {
+//             printf("*");
+//         }
+        
+//         printf("\n");
+//     }
+    
+//     return 0;
+// }
+
+
 #include <stdio.h>
 
 int main(){
-    int n;
-    scanf("%d", &n);
-    for (int i = 0; i < (n*2)-2; i+=2)
+
+    int n=5;
+
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 7; j >= i; j-=2)
+        for (int j = 0; j < n; j++)
         {
-            printf(" ");
+            if ((i+j) >= n)
+            {
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
         }
-        for (int k = 0; k <= i; k++)
+        for (int j = 0; j < n-1; j++)
         {
-            printf("*");
+            if ((i-1) >= j)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
         }
-        
         printf("\n");
     }
 
-    for (int i = (n*2)-2; i >= 0 ; i-=2)
+    for (int i = 0; i < n-1; i++)
     {
-        for (int j = 7; j >= i; j-=2)
+        for (int j = 0; j < n-1; j++)
         {
-            printf(" ");     
-        }
-        for (int k = 0; k <= i; k++)
-        {
-            printf("*");
+            if (j>=i)
+            {
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
         }
         
+        for (int j = 0; j <= n-3; j++)
+        {
+            if ((i+j)< n-3)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }   
+        }
         printf("\n");
     }
     
+    
+
     return 0;
 }
