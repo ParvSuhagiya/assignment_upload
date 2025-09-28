10486350e74abfaf9b066d8478acb4d7c6b275b0
ParvSuhagiya <parv.suhagiya.cg@gmail.com>
2025-09-28 20:56:38 +0530

    added q-7,q-8,q-9

--- c_assignments/pattern/q-40.c
+++ c_assignments/pattern/q-40.c
@@ -40,3 +40,34 @@ int main(){
     return 0;
 }
 
+
+// in 2 loops
+
+// #include <stdio.h>
+
+// int main(){
+
+//     int n;
+//     scanf("%d", &n);
+
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j < n*2 ; j++)
+//         {
+//             if (i+j == n+1 || j-i == n-1 || i == n)
+//             {
+//                 printf("*");
+//             }
+//             else
+//             {
+//                 printf(" ");
+//             }
+//         }
+        
+
+//         printf("\n");
+        
+//     }
+
+//     return 0;
+// }
--- c_assignments/pattern/q-41.c
+++ c_assignments/pattern/q-41.c
@@ -1,3 +1,49 @@
+// #include <stdio.h>
+
+// int main(){
+
+//     int n;
+//     scanf("%d", &n);
+
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1 ; j < i ; j++)
+//         {
+//             printf(" ");
+//         }
+//         for (int k = 1; k <= 2*(n-i)+1; k++)
+//         {
+//             if (i == 1)
+//             {
+//                 printf("*");
+//             }
+//             else{
+//                 if (k == 1 || k == 2*(n-i)+1)
+//                 {
+//                     printf("*");
+//                 }
+//                 else
+//                 {
+//                     printf(" ");
+//                 }
+//             }
+//         }
+//         printf("\n");
+        
+//     }
+    
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+// in 2 loops
+
 #include <stdio.h>
 
 int main(){
@@ -7,25 +53,15 @@ int main(){
 
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 1 ; j < i ; j++)
+        for (int j = 1 ; j <= 2*n - 1 ; j++)
         {
-            printf(" ");
-        }
-        for (int k = 1; k <= 2*(n-i)+1; k++)
-        {
-            if (i == 1)
+            if (j==i || j==2*n-i || i==1)
             {
                 printf("*");
             }
-            else{
-                if (k == 1 || k == 2*(n-i)+1)
-                {
-                    printf("*");
-                }
-                else
-                {
-                    printf(" ");
-                }
+            else
+            {
+                printf(" ");
             }
         }
         printf("\n");
