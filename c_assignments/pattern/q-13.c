42281e17eebdf99294ead3fb3cb2fe38dd2ab2dc
ParvSuhagiya <parv.suhagiya.cg@gmail.com>
2025-10-28 10:10:18 +0530

    diwali assignments upload

--- c_assignments/pattern/q-13.c
+++ c_assignments/pattern/q-13.c
@@ -1,31 +1,26 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main(){
+// int main(){
 
-    int n;
-    scanf("%d", &n);
+//     int n;
+//     scanf("%d", &n);
 
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = n; j > i ; j--)
-        {
-            printf(" ");
-        }
-        for (int k = 1; k <= (2*i) - 1; k++)
-        {
-            printf("*");
-        }
-        printf("\n");
-        
-    }
-    
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = n; j > i ; j--)
+//         {
+//             printf(" ");
+//         }
+//         for (int k = 1; k <= (2*i) - 1; k++)
+//         {
+//             printf("*");
+//         }
+//         printf("\n");
 
-    return 0;
-}
+//     }
 
-
-
-
+//     return 0;
+// }
 
 // optimized method --->
 
@@ -40,30 +35,56 @@ int main(){
 //         {
 //             if (i+j+2 > n)
 //             {
-//                 printf("*");     
-//             }
-//             else
-//             {
-//                 printf(" ");     
-//             }
-            
-//         }
-//         for (int j = 0; j < n-1; j++)
-//         {
-//             if ((i-1)>=j)
-//             {   
 //                 printf("*");
 //             }
 //             else
 //             {
 //                 printf(" ");
 //             }
-            
 
 //         }
-        
+//         for (int j = 0; j < n-1; j++)
+//         {
+//             if ((i-1)>=j)
+//             {
+//                 printf("*");
+//             }
+//             else
+//             {
+//                 printf(" ");
+//             }
+
+//         }
+
 //         printf("\n");
 //     }
-    
+
 //     return 0;
 // }
+
+#include <stdio.h>
+
+int main()
+{
+
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= 2 * n - 1; j++)
+        {
+            if(i + j >= n+1 && j - i <= n - 1)
+            // if(i + j >= n + 1 && j - i <= n - 1) <--- working
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
